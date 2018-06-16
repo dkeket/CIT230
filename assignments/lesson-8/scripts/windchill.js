fc4770f3976a07c7655b8940fb6f677272186116
Deklin Keket <keketd@hotmail.com>
2018-06-16 01:38:19 -0600

    More update
    Lesson 8 updates to all pages.

--- assignments/lesson-8/scripts/windchill.js
+++ assignments/lesson-8/scripts/windchill.js
@@ -6,16 +6,13 @@ var low = 70;
 var w = 4;
 */
 
-var t = (89+ 70) / 2;
+var t = (89 + 70) / 2;
 var a = 35.74 + 0.6215 * t;
 var b = 35.75 * Math.pow(4, 0.16);
 var c = 0.4275 * t * Math.pow(4, 0.16);
 var d = a - b + c;
 
-document.getElementById("windChill").innerHTML = parseFloat(d.toFixed(0));
-
-
-
+document.getElementsByClassName("windChill").innerHTML = parseFloat(d.toFixed(0));
 
 
 //Add a windchill.js file to your script folder on the weather site that uses the wind speed and the average temperature between the high and the low predictions to compute and display the wind chill temperature as a new weather data item on to display with the rest of the data. where f is the wind chill factor in Fahrenheit, t is the air average temperature in Fahrenheit, and s is the wind speed in miles per hour. 
