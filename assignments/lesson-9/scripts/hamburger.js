56206918560d4f5d33b150006cb9277e6456c6eb
Deklin Keket <keketd@hotmail.com>
2018-06-25 11:47:00 -0600

    Json updates

--- assignments/lesson-9/scripts/hamburger.js
+++
@@ -1,3 +0,0 @@
-function toggleNavMenu() {
-document.getElementById("PrimaryNav").classList.toggle("hide")
-}
---
+++ assignments/lesson-9/weatherapi.js
@@ -0,0 +1,12 @@
+var weatherObject = new XMLHttpRequest();
+weatherObject.open('GET', 'http://api.openweathermap.org/data/2.5/forecast?id=4156210&appid=49ff59e45f2f814c5595aee9ab322e82&units=imperial',true);
+
+weatherObject.send();
+
+weatherObject.onload =  function() {
+    var weatherInfo = JSON.parse(weatherObject.responseText);
+        console.log(weatherInfo);
+    document.getElementById("current-temp").innerHTML= weatherInfo.list["0"].main.temp;
+} // end function
+
+
